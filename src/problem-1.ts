9b0b95a3a17cee00cdecd38651dc08c909a05c5f
intshawrov <intshawrov@gmail.com>
2026-08-21 00:57:21 +0600

    Add Problem 1

---
+++ src/problem-1.ts
@@ -0,0 +1,20 @@
+
+const getSignalStatus = (percentage:number): string =>{
+    if(percentage >= 0 && percentage <= 25){
+        return "Poor";
+    }else if(percentage >= 26 && percentage <= 55){
+        return "Fair";
+    }else if(percentage >= 56 && percentage <=85){
+        return "Good";
+    }
+
+    return "Excellent";
+}
+
+console.log(getSignalStatus(15));
+console.log(getSignalStatus(40));
+console.log(getSignalStatus(70));
+console.log(getSignalStatus(95));
+
+
+
